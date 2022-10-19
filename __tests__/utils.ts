19c2c09e1d6a4fdffbea24cc25423e9c45910c84
Fansy <418536538@qq.com>
2022-10-20 00:42:13 +0800

    feat: upgrade to v2

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,5 +1,44 @@
-import { Middleware } from 'fatcher';
-import { getStringStreamByLength } from './getStringStreamByLength';
+import { defineMiddleware, Middleware } from 'fatcher';
+
+export function getRandomString(length: number) {
+    const STRING_TEMPLATE = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+
+    let string = '';
+
+    for (let i = 0; i < length; i++) {
+        string += STRING_TEMPLATE.charAt(Math.floor(Math.random() * STRING_TEMPLATE.length));
+    }
+
+    return string;
+}
+
+export function getStringStream(string: string, chunkSize: number) {
+    const textEncoder = new TextEncoder();
+
+    let index = 0;
+
+    return new ReadableStream<Uint8Array>({
+        start(controller) {
+            (function push() {
+                const currentText = string.slice(index * chunkSize, (index + 1) * chunkSize);
+
+                if (!currentText) {
+                    controller.close();
+                    return;
+                }
+
+                index++;
+
+                controller.enqueue(textEncoder.encode(currentText));
+                push();
+            })();
+        },
+    });
+}
+
+export function getStringStreamByLength(length: number, chunkSize: number) {
+    return getStringStream(getRandomString(length), chunkSize);
+}
 
 interface Options {
     customName?: string;
@@ -31,16 +70,14 @@ export function responser(options: Options = {}): Middleware {
         statusText: 'ok',
     });
 
-    return {
-        name: 'fatcher-middleware-responser',
-        use() {
-            return {
-                status: response.status,
-                statusText: response.statusText,
-                headers: response.headers,
-                data: response,
-                url: response.url,
-            };
-        },
-    };
+    return defineMiddleware(() => {
+        return {
+            status: response.status,
+            statusText: response.statusText,
+            headers: response.headers,
+            data: response,
+            url: response.url,
+            options: {},
+        };
+    });
 }
